64d824a0cf9e5a8b770e29f7c9783f5c64cf7e77
Shashank Agarwal <shashank.agarwal@sindia.co.in>
2024-03-17 12:07:48 +0530

    add: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { vitePluginGhPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    vitePluginGhPages()],
+  plugins: [react(),],
+  base: "/Gagan-Space.github.io/"
 })
