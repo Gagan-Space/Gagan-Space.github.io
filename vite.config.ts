c5d7b911d60988b17faa1c231ad92544fc60c04c
Witty-Wizard <Witty-Wizard@users.noreply.github.com>
2024-03-17 12:58:14 +0000

    deploy: 64d824a0cf9e5a8b770e29f7c9783f5c64cf7e77

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vitePluginGhPages } from 'vite-plugin-gh-pages';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    vitePluginGhPages()],
-})
